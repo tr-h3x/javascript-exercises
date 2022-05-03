c3b46b8ad443c326b236f9ac618bde57e264dc22
tr_h3x <travis.hetzel@gmail.com>
2022-05-03 16:19:52 -0700

    Write function to repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+//const repeatString = function repeatString( str, num) {
+  //   while (num > 0) {
+    //     if (num <= 0) {
+      //       return "error";
+        // } else {
+          //  return str.repeat(num); 
+      //      }
+     //   }
+ //
 
-};
+ const repeatString = function repeatString(str, num) {
+     if (num > 0) {
+         return str.repeat(num); 
+     } else if (num <= 0) {
+            return 'Number must be greater than 0';
+        } else {
+            return '';
+        }
+    
+    
+     }
+console.log(repeatString("hey", 0));
 
 // Do not edit below this line
 module.exports = repeatString;
