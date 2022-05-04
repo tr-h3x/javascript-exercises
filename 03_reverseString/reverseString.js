0e517f510a5f69f536bdc340d842baa01f829197
tr_h3x <travis.hetzel@gmail.com>
2022-05-04 12:36:52 -0700

    Write function to display string backwards

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    splitString = str.split("");
+    reverseArray = splitString.reverse();
+    joinArray = reverseArray.join("");
+    return joinArray;
 
 };
+
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
