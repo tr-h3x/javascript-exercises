6f0770c266395ba56f66cb5f15b0b64dddb3b5f8
tr_h3x <travis.hetzel@gmail.com>
2022-05-11 13:41:20 -0700

    Program converter for C to F & vice versa.
    Code two seperate functions. Ensure they pass .spec.js tests
    Use Math.round instead of .toFixed(1)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,37 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = (temp - 32) * (5/9);
+  let rounded = Math.round((newTemp + Number.EPSILON) * 10)/10;
+  return rounded;
 };
 
-const ctof = function() {
+//console.log(ftoc(32));
 
+const ctof = function(temp) {
+  newTemp = ((temp * (9/5)) + 32);
+  rounded = Math.round((newTemp + Number.EPSILON) * 10)/10;
+  return rounded;
 };
 
+//console.log(ctof(0));
+
+
+
+/*const ftoc = function(temp) {
+  let newTemp = (temp - 32) * (5/9);
+  return newTemp.toFixed(1);
+};
+console.log(ftoc(32));
+
+const ctof = function(temp) {
+  newTemp = ((temp * (9/5)) + 32);
+  return newTemp.toFixed(1);
+};
+console.log(ctof(0));
+
+//fails tests in .spec.js but prints to console as expected
+//perhaps returning wrong object type
+//TESTS PASSED --- required Math.round() !().toFixed(1)
+*/
 // Do not edit below this line
 module.exports = {
   ftoc,
